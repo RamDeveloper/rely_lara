131e08463288586c1b53538ea2c0011203ded6ea
ramdeveloper <ram1489@gmail.com>
2019-07-30 12:37:41 +0530

    Updated the frontend home page updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,26 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Home page</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Home page
-                </div>
-            </div>
+        <div class="col-md-3 p-5">
+            <img class="rounded-circle" src="https://instagram.fmaa1-1.fna.fbcdn.net/vp/d4e6e0aa7e84eb20623555ca685b8108/5DE56A3A/t51.2885-19/10448850_719759668083011_1824090024_a.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net">
         </div>
+        <div class="col-md-9">
+        <div class="d-flex pt-5">
+        <h1>ram.kumarz</h1>
+        <a class="pt-2 pl-3" rel="nofollow" href="/accounts/login/?next=%2Fram.kumarz%2F&amp;source=follow"><button class="_0mzm- sqdOP  L3NKy       " type="button">Follow</button></a>
+        </div>
+        <ul class="d-flex list-unstyled">
+        <li class="pr-5"><a href=""><strong>32</strong> posts</a></li>
+        <li class="pr-5" ><a href=""><strong>206</strong> followers</a></li>
+        <li class="pr-5"><a  href=""><strong>441</strong> following</a></li>
+        </ul>
+        <div><h4 >Ramkumar Subramaniam</h4><span>Techie 👨‍💻 | FZ🏍️ | Gryffindor🔥| Joker🃏 | TN-64 🏡</span> </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+    <div class="v1Nh3 kIKUG  _bz0w"><a href="/p/u2oEBTl4cT/"><div class="eLAPa"><div class="KL4Bh"><img alt="#kitty #office #raining #shelter" class="FFVAD" decoding="auto" style="object-fit: cover;" sizes="293px" srcset="https://instagram.fmaa1-1.fna.fbcdn.net/vp/b69775efe94218e48c8285d8b981ff10/5DE049F8/t51.2885-15/e15/s150x150/10784820_532867060190635_1509292652_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 150w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/f5e7bdf4c80f85f189a1ff3a3d738e69/5DEBA5A7/t51.2885-15/e15/s240x240/10784820_532867060190635_1509292652_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 240w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/038dd90c029b04da3f89bc0e5650a3f3/5DE8927C/t51.2885-15/e15/s320x320/10784820_532867060190635_1509292652_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 320w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/3824052c937a6b3067b1b46d42b1c949/5DD9186D/t51.2885-15/e15/s480x480/10784820_532867060190635_1509292652_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 480w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/bd71cfcb73ab8d25a1afdaad415be0d5/5DC994B6/t51.2885-15/e15/10784820_532867060190635_1509292652_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 640w" src="https://instagram.fmaa1-1.fna.fbcdn.net/vp/bd71cfcb73ab8d25a1afdaad415be0d5/5DC994B6/t51.2885-15/e15/10784820_532867060190635_1509292652_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net"></div><div class="_9AhH0"></div></div></a></div><div class="v1Nh3 kIKUG  _bz0w"><a href="/p/sqgMMYl4fP/"><div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" style="object-fit: cover;" sizes="293px" srcset="https://instagram.fmaa1-1.fna.fbcdn.net/vp/4b3954603336ac5612197315e27e5caf/5DCC20E3/t51.2885-15/e15/s150x150/10665479_276532585877985_1386185377_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 150w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/a28ad38f1fc5a8760ed6aedbf69a19cf/5DE9F3BC/t51.2885-15/e15/s240x240/10665479_276532585877985_1386185377_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 240w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/4c66a0f632471d960372faed80ebd563/5DED4B67/t51.2885-15/e15/s320x320/10665479_276532585877985_1386185377_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 320w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/5c63d8ad21c3bcd8146bba675fa6a1a8/5DCFEE76/t51.2885-15/e15/s480x480/10665479_276532585877985_1386185377_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 480w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/a3070a85bc03d39812060b55de0cd02f/5DCA9FAD/t51.2885-15/e15/10665479_276532585877985_1386185377_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 640w" src="https://instagram.fmaa1-1.fna.fbcdn.net/vp/a3070a85bc03d39812060b55de0cd02f/5DCA9FAD/t51.2885-15/e15/10665479_276532585877985_1386185377_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net"></div><div class="_9AhH0"></div></div></a></div><div class="v1Nh3 kIKUG  _bz0w"><a href="/p/qwXeD8l4a-/"><div class="eLAPa"><div class="KL4Bh"><img alt="#Home #terrace #temple 😊 🙏" class="FFVAD" decoding="auto" style="object-fit: cover;" sizes="293px" srcset="https://instagram.fmaa1-1.fna.fbcdn.net/vp/d81a042d891aa8872209e1136c294a5d/5DCFF1E5/t51.2885-15/e15/s150x150/927723_279545588899646_282041407_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 150w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/555efded806cd8a7395d9b2442dbf03b/5DCCA5CE/t51.2885-15/e15/s240x240/927723_279545588899646_282041407_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 240w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/146accf2404348b91123df8ee6f47c50/5DE9179C/t51.2885-15/e15/s320x320/927723_279545588899646_282041407_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 320w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/47cd0c4d6020d16474de82b2920bb1a0/5DE48A5D/t51.2885-15/e15/s480x480/927723_279545588899646_282041407_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 480w,https://instagram.fmaa1-1.fna.fbcdn.net/vp/9f5611311eedb5d75e0495575858cac5/5DEB8A14/t51.2885-15/e15/927723_279545588899646_282041407_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net 640w" src="https://instagram.fmaa1-1.fna.fbcdn.net/vp/9f5611311eedb5d75e0495575858cac5/5DEB8A14/t51.2885-15/e15/927723_279545588899646_282041407_n.jpg?_nc_ht=instagram.fmaa1-1.fna.fbcdn.net"></div><div class="_9AhH0"></div></div></a></div>
+    
     </div>
 </div>
 @endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -22,10 +22,13 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+            <div class="container d-flex align-items-center">
+                <div> <img class="pr-2" src="img/rely_logo.jpg" style="height:30px;border-right:2px solid #555"></div>
+                <div class="pl-3">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'RelyLara') }}
+                    {{ 'RelyLara'}}
                 </a>
+                </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
