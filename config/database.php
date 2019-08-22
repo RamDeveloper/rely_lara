d19b188d73c5f5cecd6e62242ad6837d7180c958
Ramkumar <ram1489@gmail.com>
2019-08-22 17:24:52 +0530

    update postgres database connection

--- config/database.php
+++ config/database.php
@@ -65,9 +65,9 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => $DATABASE_URL["host"],
-            'host' => $DATABASE_URL["port"],
-            'port' => env('DB_PORT', '5432'),
+            'url' => $DATABASE_URL,
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
